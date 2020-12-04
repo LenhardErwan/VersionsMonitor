c1f48185b8912f4c553303d6b1b23c819d047dd1
CastelJeremy <39135270+CastelJeremy@users.noreply.github.com>
2020-12-04 14:49:28 +0100

    feat: cleared initial files

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
 
-export const App = () => (
-	<div>
-		<h1>Welcome to Meteor!</h1>
-		<Hello />
-		<Info />
-	</div>
-);
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Welcome to Meteor!</h1>
+			</div>
+		);
+	}
+}
