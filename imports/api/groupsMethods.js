f3be2d17e9a483c8b8553af2991577efdb86a012
LenhardErwan <35600901+LenhardErwan@users.noreply.github.com>
2021-01-30 23:56:57 +0100

    feat: Create methods for Groups

--- imports/api/groupsMethods.js
+++ imports/api/groupsMethods.js
@@ -4,14 +4,66 @@ import GroupsCollection from '/imports/db/GroupsCollection';
 
 Meteor.methods({
 	'group.insert'(group) {
-		GroupsCollection.insert(group);
+		//TODO create canCreateRole permisison ? Create if canManage permission ?
+		if (Meteor.call('user.isAdmin', this.userId)) {
+			GroupsCollection.insert(group);
+		} else {
+			throw new Meteor.Error(
+				'perms.group.insert',
+				"You don't have permission to create a group!"
+			);
+		}
 	},
-	'group.update'(groupId, group) {
-		GroupsCollection.update(groupId, {
-			$set: group,
-		});
+	'group.update'(groupId, groupParams) {
+		const user_priority = Meteor.call(
+			'user.group.higher.priority',
+			this.userId
+		);
+		const group = GroupsCollection.findOne({ _id: groupId });
+		if (group) {
+			if (
+				user_priority < group.priority ||
+				Meteor.call('user.isAdmin', this.userId)
+			) {
+				GroupsCollection.update(groupId, {
+					$set: groupParams,
+				});
+			} else {
+				throw new Meteor.Error(
+					'perms.group.insert',
+					"You don't have permission to create a group!"
+				);
+			}
+		} else {
+			throw new Meteor.Error(
+				'group.exist',
+				`Group with id '${groupId} does not exist'!`
+			);
+		}
 	},
 	'group.delete'(groupId) {
-		MonitorsCollection.remove(groupId);
+		const user_priority = Meteor.call(
+			'user.group.higher.priority',
+			this.userId
+		);
+		const group = GroupsCollection.findOne({ _id: groupId });
+		if (group) {
+			if (
+				user_priority < group.priority ||
+				Meteor.call('user.isAdmin', this.userId)
+			) {
+				GroupsCollection.remove(groupId);
+			} else {
+				throw new Meteor.Error(
+					'perms.group.insert',
+					"You don't have permission to create a group!"
+				);
+			}
+		} else {
+			throw new Meteor.Error(
+				'group.exist',
+				`Group with id '${groupId} does not exist'!`
+			);
+		}
 	},
 });
