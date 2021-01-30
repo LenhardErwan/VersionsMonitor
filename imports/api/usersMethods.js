007d1eb316828711baabc3ca3119c3ccbb4dfbfb
LenhardErwan <35600901+LenhardErwan@users.noreply.github.com>
2021-01-30 16:17:22 +0100

    feat: method for create perm + fix: isAdmin method

--- imports/api/usersMethods.js
+++ imports/api/usersMethods.js
@@ -7,9 +7,14 @@ Meteor.methods({
 		const group_names = Meteor.users.findOne(
 			{ _id: this.userId },
 			{ fields: { _id: 0, groups: 1 } }
-		).groups;
+		);
+
+		if (!group_names.groups) {
+			throw new Meteor.Error('user.connected', 'You are not connected');
+		}
+
 		const groups = GroupsCollection.find({
-			name: { $in: group_names },
+			name: { $in: group_names.groups },
 		}).fetch();
 
 		return groups.some((group) => {
@@ -28,5 +33,29 @@ Meteor.methods({
 		return groups.some((group) => {
 			group.manageGroups;
 		});
+	},
+	'user.canCreate.nomulti'() {
+		const group = GroupsCollection.findOne({ name: this.userId });
+
+		return group.canCreate;
+	},
+	'user.canCreate.multi'() {
+		const group_names = Meteor.users.findOne(
+			{ _id: this.userId },
+			{ fields: { _id: 0, groups: 1 } }
+		).groups;
+		const groups = GroupsCollection.find({
+			name: { $in: group_names },
+		}).fetch();
+
+		const validGroups = groups.filter((group) => {
+			let isValid = false;
+			if (group.multi) {
+				isValid = group.canCreate;
+			}
+			return isValid;
+		});
+
+		return validGroups;
 	},
 });
