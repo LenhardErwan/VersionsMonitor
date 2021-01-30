3f0c18e1589915d62a4eca3070c6aadede2a9245
LenhardErwan <35600901+LenhardErwan@users.noreply.github.com>
2021-01-30 11:40:07 +0100

    fix: Delete useless code

--- imports/api/monitorsPublications.js
+++ imports/api/monitorsPublications.js
@@ -4,23 +4,6 @@ import { check } from 'meteor/check';
 import MonitorsCollection from '/imports/db/MonitorsCollection';
 import GroupsCollection from '/imports/db/GroupsCollection';
 
-// Meteor.publish('monitors.list', function (group_names) {
-// 	check(group_names, [String]);
-
-// 	const groups = GroupsCollection.find(
-// 		{ name: { $in: group_names } },
-// 		{ '_id': 0, 'monitorPerms.monitor_id': 1 }
-// 	);
-// 	const monitorIds = new Array();
-// 	for (const group of groups) {
-// 		for (const perm of group.monitorPerms) {
-// 			if (perm.canView) monitorIds.push(perm.monitor_id);
-// 		}
-// 	}
-
-// 	return MonitorsCollection.find({ _id: { $in: monitorIds } });
-// });
-
 Meteor.publish('monitors.list', function (group_names) {
 	check(group_names, [String]);
 
