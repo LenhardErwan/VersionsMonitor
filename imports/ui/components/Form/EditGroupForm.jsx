04597fb54284feaf7ad57607bb58ff32ab0f8eb1
CastelJeremy <39135270+CastelJeremy@users.noreply.github.com>
2021-01-31 14:56:44 +0100

    feat: logic to create and edit group feat: add default admin user with default admin group

--- imports/ui/components/Form/EditGroupForm.jsx
+++ imports/ui/components/Form/EditGroupForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 import {
 	Button,
 	Dialog,
@@ -10,12 +11,70 @@ import SaveIcon from '@material-ui/icons/Save';
 import { group } from '/imports/db/schemas/group';
 import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
 import { AutoFields, AutoForm, ErrorsField } from 'uniforms-material';
+import { toast } from 'react-toastify';
 
 const bridge = new SimpleSchema2Bridge(group);
 
 class EditGroupForm extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	onSubmit(newGroup) {
+		if (this.props.group._id == null) {
+			Meteor.call('groups.insert', newGroup, (err, res) => {
+				if (err) {
+					if (err.error === 'perms.groups.insert') {
+						toast.error(err.reason);
+					} else {
+						toast.error('Something went wrong, try again later!');
+					}
+				} else {
+					toast.success(newGroup.name + ' was successfully created!');
+				}
+			});
+
+			this.props.closeModal();
+		} else {
+			let oldGroup = this.props.group;
+
+			if (oldGroup !== newGroup) {
+				if (oldGroup._id === newGroup._id) {
+					let updatedGroup = {};
+
+					for (let key of Object.keys(newGroup)) {
+						if (oldGroup[key] !== newGroup[key]) {
+							updatedGroup[key] = newGroup[key];
+						}
+					}
+
+					Meteor.call(
+						'groups.update',
+						oldGroup._id,
+						updatedGroup,
+						(err, res) => {
+							if (err) {
+								if (err.error === 'perms.groups.update') {
+									toast.error(err.reason);
+								} else {
+									toast.error('Something went wrong, try again later!');
+								}
+							} else {
+								toast.success(newGroup.name + ' was successfully updated!');
+							}
+						}
+					);
+
+					this.props.closeModal();
+				} else {
+					toast.error('Something went wrong, try again later!');
+				}
+			} else {
+				toast.error('Nothing to save!');
+			}
+		}
 	}
 
 	render() {
@@ -32,7 +91,8 @@ class EditGroupForm extends React.Component {
 					<AutoForm
 						schema={bridge}
 						model={this.props.group}
-						ref={(ref) => (formRef = ref)}>
+						ref={(ref) => (formRef = ref)}
+						onSubmit={this.onSubmit}>
 						<AutoFields />
 						<ErrorsField />
 					</AutoForm>
